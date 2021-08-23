2157196bf02c9bd8b0e2b797b4732468a15d88b0
coderodde <coderodd3@gmail.com>
2021-08-23 11:24:58 +0300

    Add XMLHttRequest to the weblog index.html

---
+++ viewCounter.js
@@ -0,0 +1,29 @@
+function process() {
+    let span = document.getElementById("view_count_span");
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            renderResults(this.responseText, span);
+        }
+    };
+    xhttp.open("GET", "countView", true);
+    xhttp.send();
+}
+
+function renderResults(jsonText, textElement) {
+    const responseObject = JSON.parse(jsonText);
+    
+    if (responseObject["succeeded"]) {
+        const numberOfViews = responseObject["numberOfViews"];
+        const mostRecentViewTime = responseObject["mostRecentViewCount"];
+        textElement.innerHTML = 
+            "Total views: " +
+            numberOfViews + 
+            ". Last visit time: " + 
+            mostRecentViewTime +
+            ".";
+            
+    } else {
+        textElement.innerHTML = "Total views: N/A. Last visit time: N/A.";
+    }
+}
