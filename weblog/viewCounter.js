d2250408bab63349b471b589b5fcd8b5105f0bfd
coderodde <coderodd3@gmail.com>
2021-08-23 15:45:26 +0300

    Almost ready

--- weblog/viewCounter.js
+++ weblog/viewCounter.js
@@ -4,10 +4,7 @@ function processViewCounter() {
 
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            console.log("yees!");
             renderResults(this.responseText, span);
-        } else {
-            console.log("nooo!");
         }
     };
 
